a379ac6bb70a61367f20f5c8ef71b524b28b386b
Coyote1983hg <153317330+Coyote1983hg@users.noreply.github.com>
2024-07-17 09:01:55 +0200

    commit start animation

--- lib/src/features/shopping/presentation/start_screen.dart
+++ lib/src/features/shopping/presentation/start_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_project_baws/src/features/authentification/presentation/signin_screen.dart';
+import 'dart:math' show pi, sin, cos;
 import 'package:my_project_baws/src/features/shopping/presentation/home_screen.dart';
 
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -14,7 +15,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(const Duration(seconds: 5)).then((_) => Navigator.push(
+      Future.delayed(const Duration(seconds: 15)).then((_) => Navigator.push(
           context, MaterialPageRoute(builder: (context) => HomeScreen())));
     });
   }
@@ -22,25 +23,27 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('We are here for you !'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ShimmeringText('We are here for you !'),
       ),
       body: Stack(
         children: [
-          const AnimatedWawbaws(),
+          AnimatedBackground(child: AnimatedWawbaws()),
           Positioned(
             right: 16,
             bottom: 16,
             child: TextButton(
               onPressed: () {
-                // Navigare către HomePage
                 Navigator.of(context).pushReplacement(
                   MaterialPageRoute(builder: (context) => HomeScreen()),
                 );
               },
               child: const Text(
                 'Go to Home',
-                style: TextStyle(fontSize: 18),
+                style: TextStyle(fontSize: 18, color: Colors.white),
               ),
             ),
           ),
@@ -50,82 +53,134 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-// Adăugăm o clasă simplă pentru HomePage
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ShimmeringText extends StatefulWidget {
+  final String text;
+
+  ShimmeringText(this.text);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Center(
-        child: Text('Welcome to the Home Page!'),
-      ),
-    );
-  }
+  _ShimmeringTextState createState() => _ShimmeringTextState();
 }
 
-class AnimatedWawbaws extends StatefulWidget {
-  const AnimatedWawbaws({super.key});
-
-  @override
-  _AnimatedWawbawsState createState() => _AnimatedWawbawsState();
-}
-
-class _AnimatedWawbawsState extends State<AnimatedWawbaws>
-    with TickerProviderStateMixin {
-  late List<AnimationController> _controllersWelcome;
-  late List<AnimationController> _controllersTo;
-  late List<AnimationController> _controllersWawbaws;
-  final List<Color> colors = [
-    Colors.red,
-    Colors.blue,
-    Colors.green,
-    Colors.yellow,
-    Colors.purple,
-    Colors.orange,
-    Colors.pink,
-  ];
+class _ShimmeringTextState extends State<ShimmeringText> with SingleTickerProviderStateMixin {
+  late AnimationController _shimmerController;
 
   @override
   void initState() {
     super.initState();
-    _controllersWelcome = List.generate(
-      'Welcome'.length,
-      (index) => AnimationController(
-        duration: const Duration(milliseconds: 1500),
-        vsync: this,
-      )..repeat(reverse: true),
-    );
-    _controllersTo = List.generate(
-      'to'.length,
-      (index) => AnimationController(
-        duration: const Duration(milliseconds: 1500),
-        vsync: this,
-      )..repeat(reverse: true),
-    );
-    _controllersWawbaws = List.generate(
-      'Wawbaws'.length,
-      (index) => AnimationController(
-        duration: const Duration(milliseconds: 1500),
-        vsync: this,
-      )..repeat(reverse: true),
+    _shimmerController = AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _shimmerController,
+      builder: (context, child) {
+        return ShaderMask(
+          shaderCallback: (bounds) {
+            return LinearGradient(
+              colors: [Colors.white, Colors.blue, Colors.white],
+              stops: [0.0, _shimmerController.value, 1.0],
+              tileMode: TileMode.clamp,
+            ).createShader(bounds);
+          },
+          child: Text(
+            widget.text,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        );
+      },
     );
   }
 
   @override
   void dispose() {
-    for (var controller in _controllersWelcome) {
-      controller.dispose();
-    }
-    for (var controller in _controllersTo) {
-      controller.dispose();
-    }
-    for (var controller in _controllersWawbaws) {
-      controller.dispose();
-    }
+    _shimmerController.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatedBackground extends StatefulWidget {
+  final Widget child;
+
+  AnimatedBackground({required this.child});
+
+  @override
+  _AnimatedBackgroundState createState() => _AnimatedBackgroundState();
+}
+
+class _AnimatedBackgroundState extends State<AnimatedBackground> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.lerp(Colors.blue[900], Colors.purple[900], _controller.value)!,
+                Color.lerp(Colors.purple[900], Colors.red[900], _controller.value)!,
+              ],
+            ),
+          ),
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
+
+class AnimatedWawbaws extends StatefulWidget {
+  const AnimatedWawbaws({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedWawbawsState createState() => _AnimatedWawbawsState();
+}
+
+class _AnimatedWawbawsState extends State<AnimatedWawbaws> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 4),
+      vsync: this,
+    )..repeat();
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
     super.dispose();
   }
 
@@ -135,79 +190,36 @@ class _AnimatedWawbawsState extends State<AnimatedWawbaws>
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: 'Welcome'.split('').asMap().entries.map((entry) {
-              int idx = entry.key;
-              String char = entry.value;
-              return AnimatedBuilder(
-                animation: _controllersWelcome[idx],
-                builder: (context, child) {
-                  return Transform.scale(
-                    scale: 0.5 + 1.0 * _controllersWelcome[idx].value,
-                    child: Text(
-                      char,
-                      style: TextStyle(
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold,
-                        color: colors[idx % colors.length],
-                      ),
-                    ),
-                  );
-                },
-              );
-            }).toList(),
-          ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: 'to'.split('').asMap().entries.map((entry) {
-              int idx = entry.key;
-              String char = entry.value;
-              return AnimatedBuilder(
-                animation: _controllersTo[idx],
-                builder: (context, child) {
-                  return Transform.scale(
-                    scale: 0.5 + 1.0 * _controllersTo[idx].value,
-                    child: Text(
-                      char,
-                      style: TextStyle(
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold,
-                        color: colors[idx % colors.length],
-                      ),
-                    ),
-                  );
-                },
-              );
-            }).toList(),
-          ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: 'Wawbaws'.split('').asMap().entries.map((entry) {
-              int idx = entry.key;
-              String char = entry.value;
-              return AnimatedBuilder(
-                animation: _controllersWawbaws[idx],
-                builder: (context, child) {
-                  return Transform.scale(
-                    scale: 0.5 + 1.0 * _controllersWawbaws[idx].value,
-                    child: Text(
-                      char,
-                      style: TextStyle(
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold,
-                        color: colors[idx % colors.length],
-                      ),
-                    ),
-                  );
-                },
-              );
-            }).toList(),
-          ),
+          _buildAnimatedText('Welcome', Colors.blue[300]!, 64),
+          SizedBox(height: 20),
+          _buildAnimatedText('to', Colors.green[300]!, 48),
+          SizedBox(height: 20),
+          _buildAnimatedText('Wawbaws', Colors.red[300]!, 72),
         ],
       ),
+    );
+  }
+
+  Widget _buildAnimatedText(String text, Color color, double fontSize) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Transform(
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.001)
+            ..rotateX(sin(_animation.value * 2 * pi) * 0.1)
+            ..rotateY(cos(_animation.value * 2 * pi) * 0.1),
+          alignment: FractionalOffset.center,
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: fontSize,
+              color: color,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      },
     );
   }
 }
